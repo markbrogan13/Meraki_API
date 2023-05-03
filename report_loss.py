e24f0cfa93a5936a6f65c52055459727b127802e
Mark Brogan <Mark.Brogan@corp.meraki.com>
2023-05-03 13:53:05 -0700

    Updated to add report to json, staging live graph

--- report_loss.py
+++ report_loss.py
@@ -1,22 +1,51 @@
-import meraki
-import json
-import os
+import meraki, json, os
+import pandas as pd
+import matplotlib.pyplot as plt
 
+'''
+    load_settings will take the Org Name and extract this data from the JSON file './settings.json'
+    @params str: "org_name"
+    @returns non-mutable str: API_KEY, ORG_ID, NETWORK_ID
+'''
 def load_settings(org_name):
-    json_file = open('./settings.json')
-    json_data = json.load(json_file)
+    json_file = open('./settings.json') # opens file
+    json_data = json.load(json_file) # load json object into mutable dict for python
+    json_file.close() # close file (no mem leaks pls)
 
     API_KEY = json_data['API_KEY']
     ORG_ID = json_data[org_name]
     NETWORK_IDS = json_data[ORG_ID]
     HYPERION_ID = NETWORK_IDS[0][1]
 
-    return API_KEY, ORG_ID, HYPERION_ID
+    return API_KEY, ORG_ID, HYPERION_ID # return needed data for helper functions
 
+'''
+    report_loss_latency will take in a Serial, IP address with default to twitch.tv
+'''
 def report_loss_latency(network_id, serial, ip_addr='151.101.130.167'):
-
     result = dashboard.devices.getDeviceLossAndLatencyHistory(serial, ip_addr, timespan=300, resolution=60)
-    print(result)
+
+    try:
+        for data in result:
+            del data['goodput']
+
+        json_data_query = open('./dashboard_report.json', 'w')
+        json_data_query.write(json.dumps(result, indent = 4))
+        json_data_query.close()
+    except KeyError:
+        print("Null Data -- Check API log")
+
+'''
+    Moving Graphing data to a new file for the ability to have live data get collected from the org loss/latency
+    Keeping in this file for visibility and later debugging
+
+    def graph_data(json_data):
+        data_frame = pd.read_json(json_data)
+        data_frame.plot()
+        plt.xlabel('Duration (minutes)')
+        plt.ylabel('Latency/Percent (ms/%)')
+        plt.show()
+'''
 
 def remove_api_logs(num_logs_keep):
     file_dir = os.listdir()
